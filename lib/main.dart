8857c8f37d09878b7703f683e5b12782502bc5a6
kimjong69 <sahilbhosale700@gmail.com>
2024-06-12 16:06:40 +0530

    added comments

--- lib/main.dart
+++ lib/main.dart
@@ -10,14 +10,18 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    //build the material app
     return MaterialApp(
+      //set the initial route
       initialRoute: 'HomePage',
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
+      //set theme of the application
       theme: ThemeData(
         scaffoldBackgroundColor: const Color(0xFFF2EBE2),
         useMaterial3: true,
       ),
+      //generated routes
       routes: {
         'HomePage': (context) => const MyHomePage(),
       },
